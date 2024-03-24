57c15a769cc17c7cfdc81aae71128d5b743436fd
SergioRuiz <sergioruizrock@gmail.com>
2024-03-24 20:45:05 -0300

    se corrigieron errores

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ app.listen(app.get('port'),()=>{
 
 app.use(cors())
 app.use(morgan('dev'))
-app.express(express.json())
-app.express(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename)
